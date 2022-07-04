f1f20067120602cea8d9ed3a99c795601e84ba05
Valya <vshulya@gmail.com>
2022-07-04 21:47:43 +0100

    fix: mongo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const express = require('express');
 const { errors } = require('celebrate');
+const { MONGO } = require('./utils/constants');
 
 const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -14,7 +15,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 app.use(express.json());
 app.use(cors);
-mongoose.connect('mongodb://localhost:27017/moviesdb', {
+mongoose.connect(MONGO, {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   autoIndex: true,
