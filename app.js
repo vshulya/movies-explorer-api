f88a76688f368dd3050596301984c2985a9d1063
Valya <vshulya@gmail.com>
2022-07-06 17:09:59 +0100

    env

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const cors = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 
-const { PORT, MONGO_URL } = process.env;
+const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/moviesdb' } = process.env;
 
 const app = express();
 app.use(express.json());
