d0593f87bd36fb390cde1168c409d12637c68264
Valya <vshulya@gmail.com>
2022-07-23 16:52:19 +0100

    fix: cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,22 +1,20 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-const allowedCors = [
-  'https://my-movies-explorer.nomoredomains.xyz',
-  'https://api.my-movies-explorer.nomoredomains.xyz',
-  'http://my-movies-explorer.nomoredomains.xyz',
-  'http://api.my-movies-explorer.nomoredomains.xyz',
-  'http://localhost:3000',
-];
+// const allowedCors = [
+//   'https://my-movies-explorer.nomoredomains.xyz',
+//   'https://api.my-movies-explorer.nomoredomains.xyz',
+//   'http://my-movies-explorer.nomoredomains.xyz',
+//   'http://api.my-movies-explorer.nomoredomains.xyz',
+//   'http://localhost:3000',
+// ];
 
 module.exports = (req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-  const { origin } = req.headers;
+  // const { origin } = req.headers;
 
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
