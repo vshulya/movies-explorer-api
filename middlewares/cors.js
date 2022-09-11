4adb165089e0bf57e601be63efc350de5f2f85e0
Valya <vshulya@gmail.com>
2022-09-11 13:11:32 +0100

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,10 +1,10 @@
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 const allowedCors = [
-  'https://moviesexplorer.nomoredomains.sbs',
-  'https://api.my-movies-explorer.nomoredomains.xyz',
-  'http://moviesexplorer.nomoredomains.sbs',
-  'http://api.my-movies-explorer.nomoredomains.xyz',
+  // 'https://moviesexplorer.nomoredomains.sbs',
+  // 'https://api.my-movies-explorer.nomoredomains.xyz',
+  // 'http://moviesexplorer.nomoredomains.sbs',
+  // 'http://api.my-movies-explorer.nomoredomains.xyz',
   'http://localhost:3000',
   'http://140.82.42.116/',
   'http://140.82.42.116/movies-explorer',
